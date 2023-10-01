c2493bb09efe773c76b4b340e1315567679a83b9
Tethi062000 <tethi.cse7.bu@gmail.com>
2023-10-01 10:16:43 +0600

    Tethi's Blog

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+------------------ For More Help -----------------
+
+join me on  Facebook : https://web.facebook.com/ProDelowar
+project credit goes to : http://www.trainingwithliveproject.com/
---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "id21334759_tethirani036");
+define("DB_PASS", "Tethi036@");
+define("DB_NAME", "id21334759_personalblog");
+define("TITLE", "Training with live project");
+define("KEYWORDS", "PHP Tutorial, JAVA Tutorial, Oracle Database");
